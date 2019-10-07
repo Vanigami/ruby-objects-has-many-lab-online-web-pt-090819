3ae2597dff585fd8dab1e78891d79c345d12320e
Vanna Winland <vgwinland@gmail.com>
2019-10-07 01:31:17 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -9,9 +9,10 @@ class Author
   def self.all 
     @@all 
   end
-  def add_post(post)
-    
   
+  def add_post(post)
+    post.author = self
+  end
   def posts 
     Post.all.select{|post| post.author = self}
   end
